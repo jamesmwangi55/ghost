accc053b564cbb07ce68cacf6f91ed313f2150e1
jamesmwangi55 <jamesmwangi55@gmail.com>
2018-02-27 12:02:19 +0300

    Enable menu

--- app/src/main/java/com/example/james/ghost/addeditdream/AddEditDreamFragment.kt
+++ app/src/main/java/com/example/james/ghost/addeditdream/AddEditDreamFragment.kt
@@ -39,6 +39,11 @@ class AddEditDreamFragment : Fragment() {
         }
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
